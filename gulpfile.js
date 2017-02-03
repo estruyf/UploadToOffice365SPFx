0a89396df0461d191c3494701712575571d582da
ElioStruyf <info@estruyf.be>
2017-02-03 08:40:03 +0100

    Updated the folder location

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ const spsync = require('gulp-spsync-creds').sync;
 build.task('upload-to-sharepoint', { 
     execute: (config) => {
         return new Promise((resolve, reject) => {
-            const folderLocation = './temp/deploy/*.js';
+            const deployFolder = require('./config/prepare-deploy.json');
+            const folderLocation = `./${deployFolder.deployCdnPath}/**/*.js`;
             return gulp.src(folderLocation)
             .pipe(spsync({
                 "username": "<username>",
